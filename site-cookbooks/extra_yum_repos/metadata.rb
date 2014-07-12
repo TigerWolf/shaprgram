f003e96d3022799eaf01f43ba0d4e156925b109c
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-07-12 21:42:25 +0930

    Add metadta

---
+++ site-cookbooks/extra_yum_repos/metadata.rb
@@ -0,0 +1,8 @@
+name             'extra_yum_repos'
+maintainer       "Daniel O'Connor"
+maintainer_email 'daniel.oconnor@gmail.com'
+license          'BSD'
+description      "Adds yum::epel"
+long_description ""
+version          '0.0.1'
+depends          'yum'
