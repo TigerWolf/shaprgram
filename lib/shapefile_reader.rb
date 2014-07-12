b4109608964de494e795330272dd35da1da56a4f
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-07-12 15:25:51 +0930

    Assume GDA94 on the input and attempt to cast to WSG84 prior to save.

--- lib/shapefile_reader.rb
+++ lib/shapefile_reader.rb
@@ -14,6 +14,24 @@ class ShapefileReader
     @target ||= Dir.mktmpdir
   end
 
+  def wgs84_factory
+wgs84_proj4 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+wgs84_wkt = <<WKT
+  GEOGCS["WGS 84",
+    DATUM["WGS_1984",
+      SPHEROID["WGS 84",6378137,298.257223563,
+        AUTHORITY["EPSG","7030"]],
+      AUTHORITY["EPSG","6326"]],
+    PRIMEM["Greenwich",0,
+      AUTHORITY["EPSG","8901"]],
+    UNIT["degree",0.01745329251994328,
+      AUTHORITY["EPSG","9122"]],
+    AUTHORITY["EPSG","4326"]]
+WKT
+
+    @factory ||= wgs84_factory = RGeo::Geographic.spherical_factory(srid: 4326, proj4:wgs84_proj4, coord_sys: wgs84_wkt)
+  end
+
   def unzippify!(path)
 
     Zip::File.open(path) do |zip_file|
@@ -28,11 +46,12 @@ class ShapefileReader
   end
 
   def parse(path, project)
-    RGeo::Shapefile::Reader.open(path) do |file|
+    factory = RGeo::Geographic.spherical_factory(srid: project.srid)
+    RGeo::Shapefile::Reader.open(path, factory: factory) do |file|
       file.each do |record|
         item =  Item.new({
           name: record.attributes[project.metadata["name_column"]],
-          point: record.geometry, 
+          point: RGeo::Feature.cast(record.geometry, factory: wgs84_factory, project: true), 
           import_data: record.attributes
         })
         project.items << item
