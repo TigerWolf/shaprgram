905b93bc9efade24f8ae2f7b309860d6f7dc52eb
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-07-13 13:32:20 +0930

    MOAR infrastructure, MOAR!

--- site-cookbooks/shaprgram/recipes/default.rb
+++ site-cookbooks/shaprgram/recipes/default.rb
@@ -38,3 +38,70 @@ end
     action :upgrade
   end
 end
+
+require 'rubygems'
+require 'json'
+Gem.clear_paths
+
+include_recipe 'logrotate'
+
+template "#{node['nginx']['dir']}/sites-available/shaprgram" do
+  source "shaprgram.erb"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :reload, 'service[nginx]'
+end
+
+nginx_site 'shaprgram' do
+  enable true
+end
+
+
+if node["newrelic"]
+  execute "quiet-monitor" do
+    command %Q{curl https://rpm.newrelic.com/accounts/#{node["newrelic"]["account_id"]}/applications/#{node["newrelic"]["shaprgram"]["application_id"]}/ping_targets/disable -X POST -H "X-Api-Key: #{node["newrelic"]["api_key"]}"}
+
+    action :run
+  end
+end
+
+artifact_deploy "shaprgram" do
+  artifact_location "git://github.com/TigerWolf/shaprgram.git"
+  deploy_to "/srv/shaprgram"
+  owner "nginx"
+  group "nginx"
+  environment({ 'RAILS_ENV' => 'production', 'RBENV_ROOT' => '/opt/rbenv' })
+  shared_directories %w{ data log pids system  assets }
+  keep 1
+  action :deploy
+  notifies :reload, 'service[nginx]'
+end
+
+%w{ tmp tmp/pids tmp/sockets }.each do |dir|
+  directory File.join("/srv/shaprgram/current", dir) do
+    user 'nginx'
+    group 'nginx'
+    mode '0755'
+    recursive true
+    action :create
+  end
+end
+
+
+if node["newrelic"]
+  execute "enable-monitor" do
+    command %Q{curl https://rpm.newrelic.com/accounts/#{node["newrelic"]["account_id"]}/applications/#{node["newrelic"]["shaprgram"]["application_id"]}/ping_targets/enable -X POST -H "X-Api-Key: #{node["newrelic"]["api_key"]}"}
+
+    action :run
+  end
+end
+
+
+# template '/srv/shaprgram/current/config/newrelic.yml' do
+#   source "newrelic.yml.erb"
+
+#   notifies :reload, 'service[nginx]'
+# end
+
+
