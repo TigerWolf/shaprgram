22ee89175e74827291a28ac59cf7873387923c2f
Chris Teague <chris@cteague.com.au>
2014-07-17 00:10:15 +0930

    Require pry in dev by default

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'pry' unless Rails.env.production?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
